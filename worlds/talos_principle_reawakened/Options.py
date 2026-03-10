8e127107425c58c264ca81f08fea2551ce945d1e
johnso48 <cjohn2369@gmail.com>
2026-03-10 08:19:42 +0000

    Removed goal requirement from the game

--- worlds/talos_principle_reawakened/Options.py
+++ worlds/talos_principle_reawakened/Options.py
@@ -2,20 +2,6 @@ from dataclasses import dataclass
 
 from Options import Choice, PerGameCommonOptions, Toggle
 
-
-class GoalRequirement(Choice):
-    """
-    What is required to complete the game.
-
-    Transcendence: Reach World C and collect all 89 tetrominoes.
-    Ascension:     Climb to the top of the tower (all 5 floors).
-    """
-    display_name = "Goal Requirement"
-    option_transcendence = 0
-    option_ascension = 1
-    default = 0
-
-
 class StartingTetrominoCount(Choice):
     """
     Number of random tetrominoes to start with.
@@ -67,7 +53,6 @@ class RandomiseBonusPuzzles(Toggle):
 
 @dataclass
 class TalosPrincipleOptions(PerGameCommonOptions):
-    goal_requirement: GoalRequirement
     starting_tetromino_count: StartingTetrominoCount
     reusable_tetrominos: ReusableTetrominos
     randomise_purple_sigils: RandomisePurpleSigils
